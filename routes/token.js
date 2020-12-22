80d72dae609737086f673411da727a1fbf3bd1ad
Fabian Oraze <oraze.fabian@icloud.com>
2020-12-22 12:45:13 +0100

    fixed issue where requesting a new access token would cause an unhandled promise warning

--- routes/token.js
+++ routes/token.js
@@ -17,15 +17,16 @@ router.post('', (req, res) => {
             const resultRows = results.rows;
             if (resultRows.length != 1) {
                 res.status(401).json({ message: "authentification failed" });
+            } else {
+                const id = resultRows[0].uid;
+                const key = process.env.JWT_KEY;
+                const tokenLife = cfg.auth.tokenLife;
+                const token = jwt.sign({ userID: id }, key, { expiresIn: tokenLife, algorithm: "HS256" });
+                res.status(200).json({
+                    "message": "authentification granted",
+                    token: token
+                });
             }
-            const id = resultRows[0].uid;
-            const key = process.env.JWT_KEY;
-            const tokenLife = cfg.auth.tokenLife;
-            const token = jwt.sign({ userID: id }, key, { expiresIn: tokenLife, algorithm: "HS256" });
-            res.status(200).json({
-                "message": "authentification granted",
-                token: token
-            });
         })
         .catch(() => {
             res.status(500).json({ message: "Error occured" });
