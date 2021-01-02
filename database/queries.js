4998575d45ee46c3b015f7b6e1a14bfbdab6cba1
Fabian Oraze <oraze.fabian@icloud.com>
2021-01-02 11:18:24 +0100

    added query for filtering all

--- database/queries.js
+++ database/queries.js
@@ -1,102 +1,111 @@
-
 function login(email) {
-    return {
-        text: 'SELECT * FROM users WHERE email = $1',
-        values: [email]
-    }
+  return {
+    text: "SELECT * FROM users WHERE email = $1",
+    values: [email],
+  };
 }
 function insertUser(uid, email, password, name) {
-    return {
-        text: 'INSERT INTO users (uid, email, password, name) VALUES ($1, $2, $3, $4)',
-        values: [uid, email, password, name]
-    }
+  return {
+    text:
+      "INSERT INTO users (uid, email, password, name) VALUES ($1, $2, $3, $4)",
+    values: [uid, email, password, name],
+  };
 }
 function getUniqueID() {
-    return {
-        text: 'SELECT MAX(uid) FROM users',
-    }
+  return {
+    text: "SELECT MAX(uid) FROM users",
+  };
 }
 function checkNewUser(email) {
-    return {
-        text: 'SELECT * FROM users WHERE users.email = $1',
-        values: [email]
-    }
+  return {
+    text: "SELECT * FROM users WHERE users.email = $1",
+    values: [email],
+  };
 }
 function getAllSpendings(uid) {
-    return {
-        text: 'SELECT * FROM spendings WHERE uid = $1',
-        values: [uid]
-    }
+  return {
+    text: "SELECT * FROM spendings WHERE uid = $1",
+    values: [uid],
+  };
 }
 function getOneSpending(uid, sid) {
-    return {
-        text: 'SELECT * FROM spendings WHERE uid = $1 AND sid = $2',
-        values: [uid, sid]
-    }
+  return {
+    text: "SELECT * FROM spendings WHERE uid = $1 AND sid = $2",
+    values: [uid, sid],
+  };
 }
 function filterDate(uid, lower, upper) {
-    return {
-        text: 'SELECT * FROM spendings WHERE uid = $1 AND date >= $2 AND date <= $3',
-        values: [uid, lower, upper]
-    }
+  return {
+    text:
+      "SELECT * FROM spendings WHERE uid = $1 AND date >= $2 AND date <= $3",
+    values: [uid, lower, upper],
+  };
 }
 function filterValue(uid, lower, upper) {
-    return {
-        text: 'SELECT * FROM spendings WHERE uid = $1 AND value >= $2 AND value <= $3',
-        values: [uid, lower, upper]
-    }
+  return {
+    text:
+      "SELECT * FROM spendings WHERE uid = $1 AND value >= $2 AND value <= $3",
+    values: [uid, lower, upper],
+  };
 }
 function filterType(uid, type) {
-    return {
-        text: 'SELECT * FROM spendings WHERE uid = $1 AND type = $2',
-        values: [uid, type]
-    }
+  return {
+    text: "SELECT * FROM spendings WHERE uid = $1 AND type = $2",
+    values: [uid, type],
+  };
 }
 function filterComment(uid, comment) {
-    return {
-        text: 'SELECT * FROM spendings WHERE uid = $1 AND type LIKE %$2%',
-        values: [uid, comment]
-    }
+  return {
+    text: "SELECT * FROM spendings WHERE uid = $1 AND type LIKE %$2%",
+    values: [uid, comment],
+  };
 }
 function getNextSID() {
-    return {
-        text: 'SELECT MAX(sid) FROM spendings',
-    }
+  return {
+    text: "SELECT MAX(sid) FROM spendings",
+  };
 }
 function insertSpending(uid, sid, value, date, type, comment) {
-    return {
-        text: 'INSERT INTO spendings(uid, sid, value, date, type, comment) VALUES($1, $2, $3, $4, $5, $6)',
-        values: [uid, sid, value, date, type, comment]
-    }
+  return {
+    text:
+      "INSERT INTO spendings(uid, sid, value, date, type, comment) VALUES($1, $2, $3, $4, $5, $6)",
+    values: [uid, sid, value, date, type, comment],
+  };
 }
 function deleteSpending(uid, sid) {
-    return {
-        text: 'DELETE FROM spendings WHERE uid = $1 AND sid = $2',
-        values: [uid, sid]
-    }
+  return {
+    text: "DELETE FROM spendings WHERE uid = $1 AND sid = $2",
+    values: [uid, sid],
+  };
 }
 function updateSpending(uid, sid, key, value) {
-    return {
-        text: `UPDATE spendings SET ${key} = $3 WHERE uid = $1 AND sid = $2`,
-        values: [uid, sid, value]
-    }
-
+  return {
+    text: `UPDATE spendings SET ${key} = $3 WHERE uid = $1 AND sid = $2`,
+    values: [uid, sid, value],
+  };
+}
+function filter(uid, lowerDate, upperDate, minValue, maxValue, categories) {
+  return {
+    text:
+      "SELECT * FROM spendings WHERE uid = $1 AND date >= $2 AND date <= $3 AND value >= $4 AND value <= $5 AND type = ANY ($6::text[])",
+    values: [uid, lowerDate, upperDate, minValue, maxValue, categories],
+  };
 }
 
 module.exports = {
-    login,
-    insertUser,
-    getUniqueID,
-    checkNewUser,
-    getAllSpendings,
-    getOneSpending,
-    filterDate,
-    filterValue,
-    filterType,
-    insertSpending,
-    getNextSID,
-    deleteSpending,
-    updateSpending,
-    filterComment,
-
-}   
+  login,
+  insertUser,
+  getUniqueID,
+  checkNewUser,
+  getAllSpendings,
+  getOneSpending,
+  filterDate,
+  filterValue,
+  filterType,
+  insertSpending,
+  getNextSID,
+  deleteSpending,
+  updateSpending,
+  filterComment,
+  filter,
+};
--- routes/filter.js
+++ routes/filter.js
@@ -1,87 +1,116 @@
-let cfg = require('../config.json')
-const express = require('express');
+let cfg = require("../config.json");
+const express = require("express");
 const router = express.Router();
 const getDb = require("../database/db").getDb;
-const queries = require('../database/queries');
-const verifyToken = require('../middleware/verifyToken');
+const queries = require("../database/queries");
+const verifyToken = require("../middleware/verifyToken");
+
+router.get("/", verifyToken, (req, res) => {
+  const db = getDb();
+  const uid = req.userData.userID;
+  const startDate = req.body.startDate;
+  const endDate = req.body.endDate;
+  const minValue = req.body.minValue;
+  const maxValue = req.body.maxValue;
+  const categories = req.body.categories;
+
+  db.query(
+    queries.filter(uid, startDate, endDate, minValue, maxValue, categories)
+  )
+    .then((results) => {
+      resultRows = results.rows;
+      if (resultRows.length < 1) {
+        res.status(404).json({ message: "no entries found" });
+      } else {
+        res.status(200).json(resultRows);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "error accessing database" });
+    });
+});
 
 router.get("/value", verifyToken, (req, res) => {
-    const db = getDb();
-    const uid = req.userData.userID;
-    const lower = req.body.lowerValue;
-    const upper = req.body.upperValue;
+  const db = getDb();
+  const uid = req.userData.userID;
+  const lower = req.body.lowerValue;
+  const upper = req.body.upperValue;
 
-    db.query(queries.filterValue(uid, lower, upper))
-        .then(results => {
-            resultRows = results.rows;
-            if (resultRows.length < 1) {
-                res.status(404).json({ message: "no entries found in given value range" });
-            } else {
-                res.status(200).json(resultRows);
-            }
-        })
-        .catch(() => {
-            res.status(500).json({ message: "error accessing database" });
-        })
+  db.query(queries.filterValue(uid, lower, upper))
+    .then((results) => {
+      resultRows = results.rows;
+      if (resultRows.length < 1) {
+        res
+          .status(404)
+          .json({ message: "no entries found in given value range" });
+      } else {
+        res.status(200).json(resultRows);
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: "error accessing database" });
+    });
 });
 
 router.get("/date", verifyToken, (req, res) => {
-    const db = getDb();
-    const uid = req.userData.userID;
-    const lowerDate = req.body.lowerDate;
-    const upperDate = req.body.upperDate;
+  const db = getDb();
+  const uid = req.userData.userID;
+  const lowerDate = req.body.lowerDate;
+  const upperDate = req.body.upperDate;
 
-    db.query(queries.filterDate(uid, lowerDate, upperDate))
-        .then(results => {
-            resultRows = results.rows;
-            if (resultRows.length < 1) {
-                res.status(404).json({ message: "no entries found in given date range" });
-            } else {
-                res.status(200).json(resultRows);
-            }
-        })
-        .catch(() => {
-            res.status(500).json({ message: "error accessing database" });
-        })
+  db.query(queries.filterDate(uid, lowerDate, upperDate))
+    .then((results) => {
+      resultRows = results.rows;
+      if (resultRows.length < 1) {
+        res
+          .status(404)
+          .json({ message: "no entries found in given date range" });
+      } else {
+        res.status(200).json(resultRows);
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: "error accessing database" });
+    });
 });
 
-
 router.get("/type", verifyToken, (req, res) => {
-    const db = getDb();
-    const uid = req.userData.userID;
-    const type = req.body.type;
+  const db = getDb();
+  const uid = req.userData.userID;
+  const type = req.body.type;
 
-    db.query(queries.filterType(uid, type))
-        .then(results => {
-            resultRows = results.rows;
-            if (resultRows.length < 1) {
-                res.status(404).json({ message: "no entries found for given type" });
-            } else {
-                res.status(200).json(resultRows);
-            }
-        })
-        .catch(() => {
-            res.status(500).json({ message: "error accessing database" });
-        })
+  db.query(queries.filterType(uid, type))
+    .then((results) => {
+      resultRows = results.rows;
+      if (resultRows.length < 1) {
+        res.status(404).json({ message: "no entries found for given type" });
+      } else {
+        res.status(200).json(resultRows);
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: "error accessing database" });
+    });
 });
 
 router.get("/comment", verifyToken, (req, res) => {
-    const db = getDb();
-    const uid = req.userData.userID;
-    const comment = req.body.comment;
+  const db = getDb();
+  const uid = req.userData.userID;
+  const comment = req.body.comment;
 
-    db.query(queries.filterComment(uid, comment))
-        .then(results => {
-            resultRows = results.rows;
-            if (resultRows.length < 1) {
-                res.status(404).json({ message: "no entries found for given type" });
-            } else {
-                res.status(200).json(resultRows);
-            }
-        })
-        .catch(() => {
-            res.status(500).json({ message: "error accessing database" });
-        })
+  db.query(queries.filterComment(uid, comment))
+    .then((results) => {
+      resultRows = results.rows;
+      if (resultRows.length < 1) {
+        res.status(404).json({ message: "no entries found for given type" });
+      } else {
+        res.status(200).json(resultRows);
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: "error accessing database" });
+    });
 });
 
 module.exports = router;
